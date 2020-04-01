bad260891a8839a6588f9cd89a2a0d59227f33bd
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-04-01 15:14:45 +0200

    created dummy test

---
+++ domain/src/test/java/com/lonesome/eurder/domain/customers/CustomerTest.java
@@ -0,0 +1,12 @@
+package com.lonesome.eurder.domain.customers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+    @Test
+    void dummyTestCustomer(){
+
+    }
+}
