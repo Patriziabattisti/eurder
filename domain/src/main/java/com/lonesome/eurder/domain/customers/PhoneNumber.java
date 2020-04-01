d927cdbca4d7200a95d60a7a3ab9404705d698f0
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-04-01 13:18:26 +0200

    add jenkins file

--- domain/src/main/java/com/lonesome/eurder/domain/customers/PhoneNumber.java
+++ domain/src/main/java/com/lonesome/eurder/domain/customers/PhoneNumber.java
@@ -1,4 +1,15 @@
 package com.lonesome.eurder.domain.customers;
 
 public class PhoneNumber {
+    private String countryPrefix;
+    private String actualNumber;
+
+    public PhoneNumber(String countryPrefix, String actualNumber) {
+        this.countryPrefix = countryPrefix;
+        this.actualNumber = actualNumber;
+    }
+
+    public String getNumber(){
+        return countryPrefix+actualNumber;
+    }
 }
